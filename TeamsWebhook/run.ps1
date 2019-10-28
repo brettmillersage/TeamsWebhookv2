3772767ad539ed6be1df20674b60e5cc676d46a5
brett.miller <brett.miller@sage.com>
2019-10-28 08:23:05 +0000

    added initial files

---
+++ TeamsWebhook/run.ps1
@@ -0,0 +1,24 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param (
+    $Request,
+    $TriggerMetadata
+)
+
+# Interact with query parameters or the body of the request.
+
+if ($Request) {
+    $status = [HttpStatusCode]::OK
+    $body = $Request
+}
+else {
+    $status = [HttpStatusCode]::BadRequest
+    $body = "Payload not received from Bot Framework"
+}
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name outputSbMsg -Value ([HttpResponseContext]@{
+    StatusCode = $status
+    Body       = $body
+})
