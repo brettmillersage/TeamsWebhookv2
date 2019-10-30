5ea2c9a58ad8a3603a1831febc84d4573b355cc9
brett.miller <brett.miller@sage.com>
2019-10-30 11:03:10 +0000

    Used the correct variable for output

--- TeamsWebhook/run.ps1
+++ TeamsWebhook/run.ps1
@@ -9,4 +9,4 @@ param (
 $Request
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name outputSbMsg -Value $output.body
+Push-OutputBinding -Name outputSbMsg -Value $Request.body
