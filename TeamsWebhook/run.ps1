786adabd6eb6880ddb658bd3fbe8b75ff71561f3
brett.miller <brett.miller@sage.com>
2019-10-30 10:56:29 +0000

    change payload to just Body of input

--- TeamsWebhook/run.ps1
+++ TeamsWebhook/run.ps1
@@ -6,9 +6,7 @@ param (
     $TriggerMetadata
 )
 
-$output = $Request
-
-$output
+$Request
 
 # Associate values to output bindings by calling 'Push-OutputBinding'.
-Push-OutputBinding -Name outputSbMsg -Value $output
+Push-OutputBinding -Name outputSbMsg -Value $output.body
